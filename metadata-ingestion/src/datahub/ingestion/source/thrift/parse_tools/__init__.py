aa2653c9b84e04df362624910dbb678cd2254dbb
Ravindra Lanka <rlanka@acryl.io>
2022-05-27 15:55:33 -0700

    Minor fix.

--- metadata-ingestion/src/datahub/ingestion/source/thrift/parse_tools/__init__.py
+++ metadata-ingestion/src/datahub/ingestion/source/thrift/parse_tools/__init__.py
@@ -1,3 +1,9 @@
-from thriftLexer import thriftLexer
-from thriftParser import thriftParser
-from thriftVisitor import thriftVisitor
+from datahub.ingestion.source.thrift.parse_tools.thriftLexer import (  # type: ignore
+    thriftLexer,
+)
+from datahub.ingestion.source.thrift.parse_tools.thriftParser import (  # type: ignore
+    thriftParser,
+)
+from datahub.ingestion.source.thrift.parse_tools.thriftVisitor import (  # type: ignore
+    thriftVisitor,
+)
